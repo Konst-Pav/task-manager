9f27f10a49b34accb84ad0b71d25248418d877e7
kpavlov <k.pavlov.reg@yandex.ru>
2024-03-05 13:15:11 +0300

    added tests for user view

--- task_manager/user/tests.py
+++ task_manager/user/tests.py
@@ -6,39 +6,76 @@ from django.core.exceptions import ObjectDoesNotExist
 
 class TestUserViews(TestCase):
 
-    def test_user_index(self):
+    def setUp(self):
+        authorized_user = User.objects.create_user(username='authorized_user')
+        self.client.force_login(authorized_user)
+        User.objects.create_user(username='Kolya')
+        User.objects.create_user(username='Tonya')
+        User.objects.create_user(username='Sonya')
+
+    def test_user_index_page_is_available(self):
         response = self.client.get(reverse('users_index'))
         self.assertEqual(response.status_code, 200)
 
-    def test_user_create(self):
-        response = self.client.get(reverse('users_create'))
-        self.assertEqual(response.status_code, 200)
-        test_user = {
-            'username': 'test_username',
-            'first_name': 'test_user',
-            'last_name': 'test_user',
-            'password1': 'Parol123',
-            'password2': 'Parol123'}
-        response = self.client.post(reverse('users_create'), data=test_user)
-        self.assertEqual(response.status_code, 302)
+    def test_user_index_data_is_correct(self):
+        response = self.client.get(reverse('users_index'))
+        data = list(User.objects.values('username'))
+        content = response.content.decode()
+        data_is_correct = False
+        for user in data:
+            if user['username'] in content:
+                data_is_correct = True
+            else:
+                data_is_correct = False
+                break
+        self.assertTrue(data_is_correct)
 
-    def test_user_update(self):
-        test_user = User.objects.create_user(username='test_username')
+    def test_user_create_page_is_available(self):
+        response = self.client.get(reverse('labels_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_create_user_created(self):
+        user_data = {
+            'username': 'Vanya',
+            'first_name': 'Vanyusha',
+            'last_name': 'Ivanov',
+            'password1': 'Parol123',
+            'password2': 'Parol123',
+        }
+        self.client.post(reverse_lazy('users_create'), data=user_data)
+        try:
+            User.objects.get(username=user_data['username'])
+            user_created = True
+        except ObjectDoesNotExist:
+            print('ObjectDoesNotExist')
+            user_created = False
+        self.assertTrue(user_created)
+
+    def test_user_update_page_is_available(self):
         response = self.client.get(reverse('users_update', args=[1]))
         self.assertEqual(response.status_code, 200)
 
-    def test_user_delete(self):
-        test_user = User.objects.create_user(username='test_username')
-        response = self.client.get(reverse_lazy('users_delete', args=[test_user.id]))
-        self.assertEqual(response.status_code, 200)
-        response = self.client.post(reverse_lazy('users_delete', args=[test_user.id]))
-        try:
-            deleted_user = User.objects.get(id=test_user.id)
-            object_deleted = False
-        except ObjectDoesNotExist:
-            object_deleted = True
-        self.assertTrue(object_deleted)
-        self.assertEqual(response.status_code, 302)
+    def test_user_update_login_required(self):
+        self.client.logout()
+        response = self.client.get(reverse('users_update', args=[1]))
+        redirect_path = f'{reverse("login_view")}?next={reverse("users_update", args=[1])}'
+        self.assertRedirects(response, expected_url=redirect_path)
 
-    def test_user_login(self):
-        pass
+    def test_user_delete_page_is_available(self):
+        response = self.client.get(reverse('users_delete', args=[1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_delete_user_deleted(self):
+        self.client.post(reverse_lazy('users_delete', args=[1]))
+        try:
+            User.objects.get(id=1)
+            label_deleted = False
+        except ObjectDoesNotExist:
+            label_deleted = True
+        self.assertTrue(label_deleted)
+
+    def test_user_delete_login_required(self):
+        self.client.logout()
+        response = self.client.get(reverse('users_delete', args=[1]))
+        redirect_path = f'{reverse("login_view")}?next={reverse("users_delete", args=[1])}'
+        self.assertRedirects(response, expected_url=redirect_path)

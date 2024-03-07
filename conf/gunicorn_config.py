d79212c9efea0b80f9db3d2a63bb681c12450045
kpavlov <k.pavlov.reg@yandex.ru>
2024-03-07 16:11:41 +0300

    update gunicorn config

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,15 @@
-command = '/home/kpavlov/projects/python-project-52/.venv/bin/gunicorn'
-pythonpath = '/home/kpavlov/projects/python-project-52/.venv/bin/python'
-bind = '62.113.97.17:8000'
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+# command = '/home/kpavlov/projects/python-project-52/.venv/bin/gunicorn'
+# pythonpath = '/home/kpavlov/projects/python-project-52/.venv/bin/python'
+# bind = '62.113.97.17:8000'
+# workers = 5
+
+bind = '127.0.0.1:8000'
 workers = 5
+user = os.getenv('user_name')
+timeout = 120

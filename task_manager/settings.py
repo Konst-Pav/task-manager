c5bd49c3abb14cdc9540cd16204d5e54ea6a3249
kpavlov <k.pavlov.reg@yandex.ru>
2024-03-14 10:24:35 +0300

    shell_plus is commented out in INSTALLED_APPS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'django_filters',
     'django_extensions',
-    'shell_plus',
+    # 'shell_plus',
     'task_manager',
     'task_manager.user',
     'task_manager.label',

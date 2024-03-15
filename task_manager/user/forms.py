063640bc807047d8d47557ac5508d5ccae207702
kpavlov <k.pavlov.reg@yandex.ru>
2024-03-15 12:12:44 +0300

    login template update

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -49,5 +49,11 @@ class RegisterUserForm(UserCreationForm):
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label=_('Username'), widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        label=_('Username'),
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': _('Username')})
+    )
+    password = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': _('Password')})
+    )

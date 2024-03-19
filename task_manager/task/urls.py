ef1da5432d8b5d25ef8cd5ee113a8e653a886b36
kpavlov <k.pavlov.reg@yandex.ru>
2024-03-19 15:47:09 +0300

    Изменен путь изменения task и label

--- task_manager/task/urls.py
+++ task_manager/task/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.TaskIndexView.as_view(), name='tasks_index'),
     path('<int:pk>/', views.TaskReadView.as_view(), name='tasks_read'),
     path('create/', views.TaskCreateView.as_view(), name='tasks_create'),
-    path('<int:pk>/edit/', views.TaskUpdateView.as_view(), name='tasks_update'),
+    path('<int:pk>/update/', views.TaskUpdateView.as_view(), name='tasks_update'),
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='tasks_delete'),
 ]

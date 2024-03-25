ce43a8f1817755c989402b667b20c7c5c9512203
kpavlov <k.pavlov.reg@yandex.ru>
2024-03-25 12:31:10 +0300

    debug

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 # DEBUG = False if os.environ.get('DEBUG') == 'False' else True
 # if os.environ.get('DEBUG') == 'True':
 #     debug_mode = True
